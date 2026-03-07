6d08cc639e1d73ed0a33390746c0551edab26122
Jiří Filip <j.f.ilip@seznam.cz>
2026-03-07 14:37:54 +0100

    Make application export the traces correctly

--- justfile
+++ justfile
@@ -1,5 +1,9 @@
 set shell := ["powershell.exe", "-c"]
 
+
+monitoring-destroy:
+    docker compose down -v
+
 monitoring:
     docker compose down
     docker compose up
--- src/learning_rag/app.py
+++ src/learning_rag/app.py
@@ -13,14 +13,29 @@ resource = Resource.create({
 provider = TracerProvider(resource=resource)
 trace.set_tracer_provider(provider)
 
-exporter = OTLPSpanExporter()
+exporter = OTLPSpanExporter("http://localhost:4318/v1/traces")
 processor = BatchSpanProcessor(exporter)
 provider.add_span_processor(processor)
 
 app = FastAPI()
 
 FastAPIInstrumentor().instrument_app(app)
+tracer = trace.get_tracer(__name__)
+
 
 @app.get("/")
-async def root():
+async def root() -> dict[str, str]:
     return {"message": "Hello World"}
+
+
+@app.get("/test-trace")
+async def test_trace() -> dict[str, str]:
+    with tracer.start_as_current_span("parent-span") as parent:
+        parent.set_attribute("app.feature", "tracing-demo")
+
+        with tracer.start_as_current_span("child-span") as child:
+            child.set_attribute("app.feature", "tracing-demo")
+            child.add_event("Doing some work...")
+
+
+    return {"message": "Created spans."}
